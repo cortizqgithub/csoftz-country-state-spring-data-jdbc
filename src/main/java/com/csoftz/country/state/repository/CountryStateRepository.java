b4ca9d7587847f069535240e1311fc2548bfe639
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2025-02-18 22:17:25 -0500

    Change to use Block Strings

--- src/main/java/com/csoftz/country/state/repository/CountryStateRepository.java
+++ src/main/java/com/csoftz/country/state/repository/CountryStateRepository.java
@@ -1,5 +1,6 @@
 package com.csoftz.country.state.repository;
 
+import com.csoftz.country.state.domain.Country;
 import com.csoftz.country.state.domain.CountryState;
 import java.util.List;
 import org.springframework.data.jdbc.repository.query.Query;
@@ -7,8 +8,10 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CountryStateRepository extends CrudRepository<CountryState, Long> {
 
-    @Query("SELECT c.id AS countryId, c.name AS countryName, s.id AS stateId, s.name AS stateName " +
-        "FROM COUNTRIES c " +
-        "JOIN STATES s ON c.id = s.country_id")
+    @Query("""
+        SELECT c.id AS countryId, c.name AS countryName, s.id AS stateId, s.name AS stateName 
+        FROM COUNTRIES c 
+        JOIN STATES s ON c.id = s.country_id
+        """)
     List<CountryState> findCountryStates();
 }
